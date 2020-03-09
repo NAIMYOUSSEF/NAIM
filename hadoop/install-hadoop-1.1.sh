77500b6bf03dd18005d4203944007cf946609229
NAIMYOUSSEF <61973419+NAIMYOUSSEF@users.noreply.github.com>
2020-03-09 06:36:44 -0700

    Update install-hadoop-1.1.sh

--- hadoop/install-hadoop-1.1.sh
+++ hadoop/install-hadoop-1.1.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
-MIRROR=http://www.gtlib.gatech.edu/pub/apache/hadoop/common/hadoop-1.1.1/
-VERSION=hadoop-1.1.1
+tar -xvf hadoop-2.7.3.tar.gz
 
-apt-get update
-apt-get -y install openjdk-7-jdk
 
 #download hadoop, untar, put in /usr/local
-cd /tmp
-wget "$MIRROR/$VERSION".tar.gz
-tar -xzf "$VERSION".tar.gz
-mv  $VERSION /usr/local
-rm "$VERSION".tar.gz
+#cd /tmp
+#wget "$MIRROR/$VERSION".tar.gz
+#tar -xzf "$VERSION".tar.gz
+#mv  $VERSION /usr/local
+#rm "$VERSION".tar.gz
 
 # create user and group
-addgroup hadoop
-useradd  -G hadoop hduser -s /bin/bash -m
+#addgroup hadoop
+#useradd  -G hadoop hduser -s /bin/bash -m
 
 # app folder; who uses this ????
-mkdir -p /app/hadoop/tmp
-chown -R hduser.hadoop /app/hadoop/tmp
-chmod 750 /app/hadoop/tmp
+#mkdir -p /app/hadoop/tmp
+#chown -R hduser.hadoop /app/hadoop/tmp
+#chmod 750 /app/hadoop/tmp
 
 #modify hadoop-env
 cd /usr/local/$VERSION/conf
